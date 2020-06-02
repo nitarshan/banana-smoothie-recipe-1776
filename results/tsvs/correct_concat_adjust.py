9e21e2b985fbf5af02862f489e314ddb8286108d
Nitarshan Rajkumar <git@nitarshan.com>
2020-06-02 16:53:41 -0400

    Fix adjustment script

--- results/tsvs/correct_concat_adjust.py
+++ results/tsvs/correct_concat_adjust.py
@@ -1,74 +1,55 @@
 import pandas as pd
 import numpy as np
 
+
+def correct_sum_norms(df):
+  sum_measures = ['complexity.log_sum_of_fro', 'complexity.log_sum_of_spec', 'complexity.log_sum_of_fro_over_margin', 'complexity.log_sum_of_spec_over_margin']
+  d = df[['hp.model_depth']] * 6 + 2
+  df[sum_measures] += np.log(d.values) - d.values 
+  return df
+
+
+def adjust_measures(df):
+  measures = [col for col in df.columns if col.startswith('complexity.') and '.params' not in col]
+  adjust1 = [col for col in measures if '.log' not in col]
+  adjust1_log = [col for col in measures if '.log' in col]
+
+  adjust1_new = [x+'_adjusted1' for x in adjust1]
+  adjust1_log_new = [x+'_adjusted1' for x in adjust1_log]
+
+  m = df[['train_dataset_size']].values
+
+  df[adjust1_new] = np.sqrt(df[adjust1] / m)
+  df[adjust1_log_new] = 0.5 * (df[adjust1_log] - np.log(m))
+
+  return df
+
+
 # CIFAR-10
-# Correct for incorrect measure calculations
-measures = ['complexity.log_sum_of_fro', 'complexity.log_sum_of_spec', 'complexity.log_sum_of_fro_over_margin', 'complexity.log_sum_of_spec_over_margin']
+df1 = []
 for dataset_size in [6_250, 12_500, 25_000, 50_000]:
-  df = pd.read_csv(f'./results/tsvs/nin_cifar10.{dataset_size}.tsv', sep='\t', index_col=0)
-  effective_depths = df[['hp.model_depth']] * 6 + 2
-  df[measures] += np.log(effective_depths.values) - effective_depths.values 
-  df.to_csv(f'./results/tsvs/nin_cifar10.{dataset_size}_corrected.tsv', sep='\t', float_format="%g")
-
-# Concatenate all dataset size tsv's into a single tsv
-dfs = []
-for dataset_size in [6_250, 12_500, 25_000, 50_000]:
-  dfs.append(pd.read_csv(f'./results/tsvs/nin_cifar10.{dataset_size}_corrected.tsv', sep='\t', index_col=0))
-pd.concat(dfs).to_csv(f'./results/tsvs/nin_cifar10_corrected.tsv', sep='\t', float_format="%g")
-
-# Adjust measures accounting for dataset size
-df = pd.read_csv('./results/tsvs/nin_cifar10_corrected.tsv', sep='\t', index_col=0)
-
-print(df.columns)
-
-adjust1 = [col for col in df.columns if col.startswith('complexity.') and '.log' not in col and '.params' not in col and '.inverse_margin' not in col]
-adjust1_log = [col for col in df.columns if col.startswith('complexity.') and '.log' in col and '.params' not in col and '.inverse_margin' not in col]
-adjust2 = [col for col in df.columns if col.startswith('complexity.') and 'path_norm' in col]
-
-adjust1_new = [x+'_adjusted1' for x in adjust1]
-adjust1_log_new = [x+'_adjusted1' for x in adjust1_log]
-adjust2_new = [x+'_adjusted2' for x in adjust2]
-print('adjust1', adjust1)
-print('adjust1_log', adjust1_log)
-print('adjust2', adjust2)
-
-d = df[['hp.model_depth']].values * 6 + 2
-m = df[['train_dataset_size']].values
-
-df[adjust1_new] = np.sqrt(df[adjust1] / m)
-df[adjust1_log_new] = 0.5 * (df[adjust1_log] - np.log(m))
-df[adjust2_new] = np.sqrt(((df[adjust2] / np.sqrt(d)) ** (2 * d)) / m)
-
-df.to_csv(f'./results/tsvs/nin_cifar10_corrected_adjusted.tsv', sep='\t', float_format="%g")
+  df1.append(pd.read_csv(f'./results/tsvs/nin_cifar10.{dataset_size}.tsv', sep='\t', index_col=0))
+df1 = pd.concat(df1)
+df1 = correct_sum_norms(df1)
+df1['hp.dataset'] = 'CIFAR10'
 
 # SVHN
-# Concatenate all dataset size tsv's into a single tsv
-dfs = []
+df2 = []
 for dataset_size in [6_250, 12_500, 25_000, 50_000]:
-  dfs.append(pd.read_csv(f'./results/tsvs/nin_svhn.{dataset_size}.tsv', sep='\t', index_col=0))
-pd.concat(dfs).to_csv(f'./results/tsvs/nin_svhn.tsv', sep='\t', float_format="%g")
+  df2.append(pd.read_csv(f'./results/tsvs/nin_svhn.{dataset_size}.tsv', sep='\t', index_col=0))
+df2 =  pd.concat(df2)
+df2['hp.dataset'] = 'SVHN'
 
-# Adjust measures accounting for dataset size
-df = pd.read_csv('./results/tsvs/nin_svhn.tsv', sep='\t', index_col=0)
+# SVHN + CIFAR-10
+df3 = pd.concat([df1, df2])
+df2 = adjust_measures(df2)
+df3['hp.train_dataset_size'] = df3['train_dataset_size']
+df3['complexity.log_spec_orig_main'] = df3['complexity.log_prod_of_spec_over_margin'] + np.log(df3['complexity.fro_over_spec'])
 
-print(df.columns)
+# Save to CSVs
+df3.to_csv(f'./results/tsvs/nin_adjusted.csv', float_format="%.4g")
 
-adjust1 = [col for col in df.columns if col.startswith('complexity.') and '.log' not in col and '.params' not in col and '.inverse_margin' not in col]
-adjust1_log = [col for col in df.columns if col.startswith('complexity.') and '.log' in col and '.params' not in col and '.inverse_margin' not in col]
-adjust2 = [col for col in df.columns if col.startswith('complexity.') and 'path_norm' in col]
-
-adjust1_new = [x+'_adjusted1' for x in adjust1]
-adjust1_log_new = [x+'_adjusted1' for x in adjust1_log]
-adjust2_new = [x+'_adjusted2' for x in adjust2]
-print('adjust1', adjust1)
-print('adjust1_log', adjust1_log)
-print('adjust2', adjust2)
-
-d = df[['hp.model_depth']].values * 6 + 2
-m = df[['train_dataset_size']].values
-
-df[adjust1_new] = np.sqrt(df[adjust1] / m)
-df[adjust1_log_new] = 0.5 * (df[adjust1_log] - np.log(m))
-df[adjust2_new] = np.sqrt(((df[adjust2] / np.sqrt(d)) ** (2 * d)) / m)
-
-df.to_csv(f'./results/tsvs/nin_svhn_adjusted.tsv', sep='\t', float_format="%g")
+df3 = pd.read_csv(f'./results/tsvs/nin_adjusted.csv')
+assert(len(df3['hp.lr'].unique())==5)
+assert(len(df3['hp.model_depth'].unique())==5)
+assert(len(df3['hp.model_width'].unique())==5)
