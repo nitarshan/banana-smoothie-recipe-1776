570323572a65da185e068b32b66c625467ab177d
Alexandre Drouin <alexandre.drouin1@gmail.com>
2020-06-16 22:54:40 -0400

    deleted scripts for anonymity

--- scripts/push.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-if [ "$1" = "mila" ]
-then
-  rsync --exclude='.git/' --exclude='*__pycache__' -ruv ./ $1:~/causal-capacity-measures/
-else
-  rsync --exclude='.git/' --exclude='*__pycache__' -ruv ./ $1:~/scratch/causal-capacity-measures/
-fi
